482cffcc2766bf6bc1b1dad2475f643ba32a5c18
VOsipenkov <VOsipenkov@lanit.ru>
2021-12-22 13:30:14 +0300

    updated dependencies, created java project with maven

---
+++ src/test/java/org/camunda/home/WeatherDegrees.java
@@ -0,0 +1,14 @@
+package org.camunda.home;
+
+import java.util.Random;
+
+/**
+ * todo Document type WeatherDegrees
+ */
+public class WeatherDegrees {
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+    }
+}
